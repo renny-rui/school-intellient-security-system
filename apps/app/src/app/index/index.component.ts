aecf3becd9069044af9a6841722d95bf3ea9ed37
lenonrade <lenonrade@163.com>
2022-01-08 23:59:16 +0800

    fix(app): amend code

--- apps/app/src/app/index/index.component.ts
+++ apps/app/src/app/index/index.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as echarts from 'echarts';
+
+type EChartsOption = echarts.EChartsOption;
 
 @Component({
   selector: 'school-intelligent-security-system-index',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  @ViewChild('echartTotal') echartTotal: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.pie();
+  }
+  pie(): void {
+    const myChart = echarts.init(this.echartTotal);
+    const option = {
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        top: '15%',
+        right: 'right'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: ['50%', '60%'],
+          avoidLabelOverlap: false,
+          label: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: '20',
+              fontWeight: 'bold'
+            }
+          },
+          labelLine: {
+            show: false
+          },
+          data: [
+            { value: 1048, name: '初中' },
+            { value: 735, name: '小学' },
+            { value: 580, name: '六年级' }
+          ]
+        }
+      ]
+    };
+    myChart.setOption(option);
   }
 
 }
